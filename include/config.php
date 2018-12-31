08497344bd68e750c12a7917ab1bff49576c1b11
mambax7 <mambax7@gmail.com>
2018-12-31 06:36:06 -0500

    add common.php, config.php

---
+++ include/config.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * @copyright    XOOPS Project https://xoops.org/
+ * @license      GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package
+ * @since
+ * @author       XOOPS Development Team
+ */
+
+
+function getConfig()
+{
+    $moduleDirName      = basename(dirname(__DIR__));
+    $moduleDirNameUpper = strtoupper($moduleDirName);
+    return (object)[
+        'name'           => strtoupper($moduleDirName) . ' Module Configurator',
+        'paths'          => [
+            'dirname'    => $moduleDirName,
+            'admin'      => XOOPS_ROOT_PATH . '/modules/' . $moduleDirName . '/admin',
+            'modPath'    => XOOPS_ROOT_PATH . '/modules/' . $moduleDirName,
+            'modUrl'     => XOOPS_URL . '/modules/' . $moduleDirName,
+            'uploadPath' => XOOPS_UPLOAD_PATH . '/' . $moduleDirName,
+            'uploadUrl'  => XOOPS_UPLOAD_URL . '/' . $moduleDirName,
+        ],
+        'uploadFolders'  => [
+            XOOPS_UPLOAD_PATH . '/' . $moduleDirName,
+            XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/category',
+            XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/screenshots',
+            //XOOPS_UPLOAD_PATH . '/flags'
+        ],
+        'copyBlankFiles' => [
+            XOOPS_UPLOAD_PATH . '/' . $moduleDirName,
+            XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/category',
+            XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/screenshots',
+            //XOOPS_UPLOAD_PATH . '/flags'
+        ],
+
+        'copyTestFolders' => [
+            //        XOOPS_UPLOAD_PATH . '/' . $moduleDirName,
+            //[
+            //    constant($moduleDirNameUpper . '_PATH') . '/testdata/images',
+            //    XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/images',
+            //]
+        ],
+
+
+        'copyTestFolders' => [
+            [
+                XOOPS_ROOT_PATH . '/modules/' . $moduleDirName . '/testdata/images',
+                XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/images',
+            ],
+            [
+                XOOPS_ROOT_PATH . '/modules/' . $moduleDirName . '/testdata/thumbs',
+                XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/thumbs',
+            ]
+        ],
+
+
+
+        'templateFolders' => [
+            '/templates/',
+//            '/templates/blocks/',
+//            '/templates/admin/'
+
+        ],
+        'oldFiles'        => [
+            '/class/request.php',
+            '/class/registry.php',
+            '/class/utilities.php',
+            '/class/util.php',
+//            '/include/constants.php',
+//            '/include/functions.php',
+            '/ajaxrating.txt',
+        ],
+        'oldFolders'      => [
+            '/images',
+            '/css',
+            '/js',
+            '/tcpdf',
+            '/images',
+        ],
+
+        'renameTables' => [
+   //         'XX_archive'     => 'ZZZZ_archive',
+        ],
+        'modCopyright'    => "<a href='https://xoops.org' title='XOOPS Project' target='_blank'>
+                     <img src='" . constant($moduleDirNameUpper . '_AUTHOR_LOGOIMG') . '\' alt=\'XOOPS Project\' /></a>',
+    ];
+}
