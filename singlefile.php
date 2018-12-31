a3ee2e9e94b98de637d56d800e52ee8f1dd2a1a5
mambax7 <mambax7@gmail.com>
2018-12-31 03:11:20 -0500

    'stristr(...)/stripos(...)/strripos(...)' could be replaced with 'strstr(...)/strpos()/strrpos()'

--- singlefile.php
+++ singlefile.php
@@ -204,7 +204,7 @@ if (1 == $xoopsModuleConfig['permission_download']) {
 if ((1 == $xoopsModuleConfig['usetellafriend']) and (is_dir('../tellafriend'))) {
     $string = sprintf(_MD_TDMDOWNLOADS_SINGLEFILE_INTFILEFOUND, $xoopsConfig['sitename'].':  '.XOOPS_URL.'/modules/tdmdownloads/singlefile.php?lid=' . $_REQUEST['lid']);
     $subject = sprintf(_MD_TDMDOWNLOADS_SINGLEFILE_INTFILEFOUND, $xoopsConfig['sitename']);
-    if (false !== stripos($subject, '%')) {
+    if (false !== strpos($subject, '%')) {
         $subject = rawurldecode($subject) ;
     }
     if (false !== stripos($string, '%3F')) {
