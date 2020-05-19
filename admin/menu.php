f29618992d3f5c05e4bc18e8aae8f53ec7b1b15a
mambax7 <mambax7@gmail.com>
2020-05-19 10:03:36 -0400

    fix install bugs

--- admin/menu.php
+++ admin/menu.php
@@ -26,54 +26,61 @@ $helper->loadLanguage('feedback');
 
 $pathIcon32 = \Xmf\Module\Admin::menuIconPath('');
 if (is_object($helper->getModule())) {
-    $pathModIcon32 = $helper->getModule()->getInfo('modicons32');
+//    $pathModIcon32 = $helper->getModule()->getInfo('modicons32');
+    $pathModIcon32 = $helper->url($helper->getModule()->getInfo('modicons32'));
 }
 
 $adminmenu[] = [
     'title' => _MI_TDMDOWNLOADS_ADMENU1,
     'link'  => 'admin/index.php',
-    'icon'  => "{$pathModIcon32}/home.png",
+//    'icon'  => "{$pathModIcon32}/home.png",
+    'icon'  => 'assets/images/admin/home.png',
 ];
 
 $adminmenu[] = [
     'title' => _MI_TDMDOWNLOADS_ADMENU2,
     'link'  => 'admin/category.php',
-    'icon'  => "{$pathModIcon32}/category.png",
-    //'menu' =>  "{$pathIcon32}/menu_category.png",
+//    'icon'  => "{$pathModIcon32}/category.png",
+    'icon'  => 'assets/images/admin/category.png',
 ];
 
 $adminmenu[] = [
     'title' => _MI_TDMDOWNLOADS_ADMENU3,
     'link'  => 'admin/downloads.php',
-    'icon'  => "{$pathModIcon32}/downloads.png",
+//    'icon'  => "{$pathModIcon32}/downloads.png",
+    'icon'  => 'assets/images/admin/downloads.png',
     //'menu' =>  "{$pathIcon32}/menu_downloads.png",
 ];
 
 $adminmenu[] = [
     'title' => _MI_TDMDOWNLOADS_ADMENU4,
     'link'  => 'admin/broken.php',
-    'icon'  => "{$pathModIcon32}/broken.png",
+//    'icon'  => "{$pathModIcon32}/broken.png",
+    'icon'  => 'assets/images/admin/broken.png',
     //'menu' =>  "{$pathIcon32}/menu_broken.png",
 ];
 
 $adminmenu[] = [
     'title' => _MI_TDMDOWNLOADS_ADMENU5,
     'link'  => 'admin/modified.php',
-    'icon'  => "{$pathModIcon32}/modified.png",
+//    'icon'  => "{$pathModIcon32}/modified.png",
+    'icon'  => 'assets/images/admin/modified.png',
     //'menu' =>  "{$pathIcon32}/menu_modified.png",
 ];
 
 $adminmenu[] = [
     'title' => _MI_TDMDOWNLOADS_ADMENU6,
     'link'  => 'admin/field.php',
-    'icon'  => "{$pathModIcon32}/field.png",
+//    'icon'  => "{$pathModIcon32}/field.png",
+    'icon'  => 'assets/images/admin/field.png',
     //'menu' =>  "{$pathIcon32}/menu_field.png",
 ];
 
 $adminmenu[] = [
     'title' => _MI_TDMDOWNLOADS_ADMENU7,
     'link'  => 'admin/import.php',
-    'icon'  => "{$pathModIcon32}/import.png",
+//    'icon'  => "{$pathModIcon32}/import.png",
+    'icon'  => 'assets/images/admin/import.png',
     //'menu' =>  "{$pathIcon32}/menu_import.png",
 ];
 
--- include/oninstall.php
+++ include/oninstall.php
@@ -25,7 +25,7 @@ function xoops_module_pre_install_tdmdownloads(\XoopsModule $module)
 {
     require_once dirname(dirname(dirname(__DIR__))) . '/mainfile.php';
 
-    require_once __DIR__ . '/common.php';
+//    require_once __DIR__ . '/common.php';
 
     /** @var \XoopsModules\Tdmdownloads\Utility $utility */
 
