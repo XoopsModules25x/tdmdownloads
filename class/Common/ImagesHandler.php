919f19683960fef8b1ee8a64ef67e8b3e605a60a
mambax7 <mambax7@gmail.com>
2021-10-29 00:13:42 -0400

    Parameter's name changed during inheritance

--- class/Common/ImagesHandler.php
+++ class/Common/ImagesHandler.php
@@ -37,13 +37,13 @@ class ImagesHandler extends \XoopsPersistableObjectHandler
     /**
      * retrieve a field
      *
-     * @param int        $i field id
+     * @param int        $id field id
      * @param null|mixed $fields
      * @return mixed reference to the {@link Get} object
      */
-    public function get($i = null, $fields = null)
+    public function get($id = null, $fields = null)
     {
-        return parent::get($i, $fields);
+        return parent::get($id, $fields);
     }
 
     /**
